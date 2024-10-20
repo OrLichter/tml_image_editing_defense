a451c308d2d9f442939d318c097e58334486d1ab
yuval-alaluf <yuvalalaluf@gmail.com>
2024-10-20 11:42:49 +0300

    Push before trying to run on multiple images

--- main.py
+++ main.py
@@ -501,10 +501,10 @@ if __name__ == '__main__':
 	)
 	adversarial_image = trainer.run()
 	adversarial_image.save(output_path / "adversarial_image.png")
-	torch.save(trainer.noise, output_path / "noise.pt")
+	torch.save(trainer.noises, output_path / "noise.pt")
 	
 	adversarial_image = Image.open(output_path / "adversarial_image.png").convert("RGB")
-	trainer.noise = torch.load(output_path / "noise.pt")
+	trainer.noises = torch.load(output_path / "noise.pt")
 	
 	# Part 2: Inference
 	inference_cfg = InferenceConfig(
